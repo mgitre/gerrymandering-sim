156a60c6aace9d1b9d74674a9f3320abb640df32
mgitre <max.fitzkin@gmail.com>
2024-02-06 23:00:13 -0500

    cleaned up a tiny lil bit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import Model from './components/model';
 function App() {
   return (
     <div className="App">
-      <Model width={Math.min(500, window.innerWidth-50)}
-      rows={9} cols={9} district_size={9} voters_interactive={false} districts_interactive={true} gridtype="random" random_distribution={[40, 41]} /*preset_district_shape={[3,3]}*/ title="Gerrymander red to power!" id={0}/>
+      <Model 
+      width={Math.min(500, window.innerWidth-50)}
+      rows={9} 
+      cols={9} 
+      district_size={9} 
+      voters_interactive={false} 
+      districts_interactive={true} 
+      gridtype="random"
+      random_distribution={[40,41]}
+      /*preset_district_shape={[3,3]}*/ 
+      title="Gerrymander red to power!" 
+      id={0}/>
     </div>
   );
 }
